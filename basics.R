554c867c164e26d6162b233a415b12171df371f5
xhri <stempowski132@gmail.com>
2019-11-10 14:45:43 +0100

    new features

--- basics.R
+++ basics.R
@@ -6,21 +6,57 @@ bp = read.csv("BP.csv")
 head(bp)
 summary(bp)
 plot(bp)
-?plot
+median(bp$systolic)
+mean(bp$systolic)
 
+?plot
 plot(bp$time_of_day, bp$systolic)
 
-?factor
 
+?factor
 bp$time_of_day_sorted <- factor(bp$time_of_day, c("morning", "midday", "evening"))
 plot(bp$time_of_day_sorted, bp$systolic)
 
 
-?median
+?table
+time_data <- table(bp$hour)
+barplot(time_data)
 
-median(bp$systolic)
-average(bp$systolic)
-mean(bp$systolic)
+time_data_plus <- table(factor(bp$hour,levels = 6:24))
+barplot(time_data_plus)
 
-head(bp)
-aggregate(bp[,4:5], list(bp$date), mean)
+
+# Compare Morning & Evening side by side
+par(mfrow=c(2,1))
+
+morning_diastolic <- bp$diastolic[bp$time_of_day=="morning"]
+morning_systolic <- bp$systolic[bp$time_of_day=="morning"]
+
+plot(morning_diastolic, morning_systolic, main="Morning", xlab="Diastolic", ylab="Systolic")
+
+evening_diastolic <- bp$diastolic[bp$time_of_day=="evening"]
+evening_systolic <- bp$systolic[bp$time_of_day=="evening"]
+
+plot(evening_diastolic, evening_systolic, main="Evening", xlab="Diastolic", ylab="Systolic")
+
+
+
+# Compare Morning & Evening side by side using function
+daypartplot <- function(time) {
+  tmp_diastolic <- bp$diastolic[bp$time_of_day==time]
+  tmp_systolic <- bp$systolic[bp$time_of_day==time]
+  
+  plot(tmp_diastolic, tmp_systolic, main=time, xlab="Diastolic", ylab="Systolic")
+}
+
+par(mfrow=c(2,1))
+daypartplot("morning")
+daypartplot("evening")
+
+
+
+# Compare Morning & Evening on 1 plot
+par(mfrow=c(1,1))
+plot(bp$diastolic,bp$systolic)
+points(bp$diastolic[bp$time_of_day=="morning"],bp$systolic[bp$time_of_day=="morning"],col="red")
+points(bp$diastolic[bp$time_of_day=="evening"],bp$systolic[bp$time_of_day=="evening"],col="blue")
